55b0831811fbadf9771d0ccc4c172ad6586502be
Иванов Евгений Константинович <genom990@mail.ru>
2018-05-11 20:27:50 +0700

    add TaskSystemControl.Infrastructure layer

---
+++ TaskControlSystem.Infrastructure/Repository/EntityRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.Data.Entity;
+
+namespace TaskControlSystem.Infrastructure.Repository
+{
+    [Export(typeof(IRepository<>))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class EntityRepository<T> : IRepository<T> where T : class
+    {
+        private readonly DbContext _dbContext;
+
+        public EntityRepository(DbContext db)
+        {
+            _dbContext = db;
+        }
+
+        public void Add(T item)
+        {
+            var dbSet = _dbContext.Set<T>();
+            dbSet.Add(item);
+        }
+
+        public void Remove(T item)
+        {
+            var dbSet = _dbContext.Set<T>();
+            dbSet.Remove(item);
+        }
+
+        public T Find(int id)
+        {
+            var dbSet = _dbContext.Set<T>();
+            var item = dbSet.Find(id);
+            return item;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            var dbSet = _dbContext.Set<T>();
+            return dbSet;
+        }
+    }
+}
---
+++ TaskControlSystem.Infrastructure/RepositoryProvider.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using Microsoft.Practices.ServiceLocation;
+using TaskControlSystem.DataAccess;
+using TaskControlSystem.Infrastructure.Repository;
+using System.Data.Entity;
+
+namespace TaskControlSystem.Infrastructure
+{
+    [Export(typeof(IRepositoryProvider))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class RepositoryProvider : IRepositoryProvider
+    {
+        private DbContext _dbContext;
+        private bool disposed = false;
+
+        public RepositoryProvider()
+        {
+            _dbContext = new TaskContext();
+        }
+
+        [Import(RequiredCreationPolicy = CreationPolicy.Shared)]
+        protected virtual IServiceLocator ServiceLocator { get; set; }
+
+        public IRepository<TEntity> GetRepository<TEntity>() where TEntity : class
+        {
+            if (!disposed)
+            {
+                var repository = new EntityRepository<TEntity>(_dbContext);
+
+                return repository;
+            }
+
+            return ServiceLocator.GetInstance<IRepository<TEntity>>();
+        }
+
+        public virtual void SaveChanges()
+        {
+            _dbContext.SaveChanges();
+            return;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    if (_dbContext != null)
+                    {
+                        _dbContext.Dispose();
+                        _dbContext = null;
+                    }
+                }
+                disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
