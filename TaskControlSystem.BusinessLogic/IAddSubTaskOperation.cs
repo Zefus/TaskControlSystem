eea9d9c9ae5fa702503857984aae3106e3c4703e
Иванов Евгений Константинович <genom990@mail.ru>
2018-05-12 16:51:50 +0700

    add AddSubTaskOperation

---
+++ TaskControlSystem.BusinessLogic/IAddSubTaskOperation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskControlSystem.DataAccess.Models;
+using TaskControlSystem.BusinessLogic.TaskViewModels;
+
+namespace TaskControlSystem.BusinessLogic
+{
+    public interface IAddSubTaskOperation
+    {
+        void Execute(SystemTask parentTask, CreateTaskViewModel childTask);
+    }
+}
---
+++ TaskControlSystem.BusinessLogic/Operations/AddSubTaskOperation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+using TaskControlSystem.DataAccess.Models;
+using TaskControlSystem.Infrastructure;
+using TaskControlSystem.BusinessLogic.TaskViewModels;
+
+namespace TaskControlSystem.BusinessLogic.Operations
+{
+    [Export(typeof(IAddSubTaskOperation))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class AddSubTaskOperation : IAddSubTaskOperation
+    {
+        [Import]
+        private IRepositoryProvider _repositoryProvider;
+
+        public void Execute(SystemTask parentTask, CreateTaskViewModel childTask)
+        {
+            var repository = _repositoryProvider.GetRepository<SystemTask>();
+
+            SystemTask task = new SystemTask
+            {
+                Title = childTask.Title,
+                Description = childTask.Descriptiion,
+                Executors = childTask.Executors,
+                Status = childTask.Status,
+                RegisterDate = childTask.RegisterDate,
+                CompletionDate = childTask.CompletionDate,
+                ParentSystemTask = parentTask
+            };
+
+            repository.Find(parentTask.Id).ChildSystemTasks.Add(task);
+            _repositoryProvider.SaveChanges();
+        }
+    }
+}
