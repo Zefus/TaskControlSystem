706fb030716c7ba5a9528d24d7b5fa97f6b7365a
Иванов Евгений Константинович <genom990@mail.ru>
2018-05-12 18:10:38 +0700

    add RemoveTaskOperation

---
+++ TaskControlSystem.BusinessLogic/IRemoveTaskOperation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaskControlSystem.DataAccess.Models;
+
+namespace TaskControlSystem.BusinessLogic
+{
+    public interface IRemoveTaskOperation
+    {
+        void Execute(SystemTask selectedTask);
+    }
+}
---
+++ TaskControlSystem.BusinessLogic/Operations/RemoveTaskOperation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+using TaskControlSystem.DataAccess.Models;
+using TaskControlSystem.Infrastructure;
+
+namespace TaskControlSystem.BusinessLogic.Operations
+{
+    [Export(typeof(IRemoveTaskOperation))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class RemoveTaskOperation : IRemoveTaskOperation
+    {
+        [Import]
+        private IRepositoryProvider _repositoryProvider;
+
+        public void Execute(SystemTask selectedTask)
+        {
+            var repository = _repositoryProvider.GetRepository<SystemTask>();
+
+            repository.Remove(selectedTask);
+
+            _repositoryProvider.SaveChanges();
+        }
+    }
+}
